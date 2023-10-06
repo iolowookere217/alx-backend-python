9d78d7eacd272ddbb1c612ed4c38b3d6b0959307
iolowookere217 <olowookere.isaaco@gmail.com>
2023-10-06 02:41:01 +0000

    removed task 0

--- 0x00-python_variable_annotations/0-add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Task 0: This module defines an annotated function `add`"""
-
-
-def add(a: float, b: float) -> float:
-    """adds two floats, returns the sum"""
-    return a + b
