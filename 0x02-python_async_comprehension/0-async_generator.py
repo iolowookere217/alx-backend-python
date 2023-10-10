cbd0ebe7f51ca45840f2b18d6d69c86bd9e8ef1e
iolowookere217 <olowookere.isaaco@gmail.com>
2023-10-10 18:22:29 +0000

    Task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,6 +9,12 @@ import random
 import asyncio
 
 
+"""float is return type of the yield
+   None means the generator wont take any parameter
+   None means no error will be raised
+"""
+
+
 async def async_generator() -> Generator[float, None, None]:
     '''A coroutine that iterates 10 times each time asynchronously
     wait 1 second and yields a random number between 0 and 10.
